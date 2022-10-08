48d5d8bc9f3cb570128ccf2cda219c82d5d77d2b
Allan Ritchie <allan.ritchie@gmail.com>
2022-10-08 17:04:58 -0400

    Fix for VS4mac

--- ProjectTemplates/ShinyApp/MauiProgram.cs
+++ ProjectTemplates/ShinyApp/MauiProgram.cs
@@ -18,6 +18,8 @@ public static class MauiProgram
             )
             .ConfigureFonts(fonts =>
             {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold"); 
             });
 
 #if useconfig
