1ba63be41c200cb1ac85c679b7d556d2b90e7edc
Allan Ritchie <allan.ritchie@gmail.com>
2022-10-17 22:42:10 -0400

    Add missing push hooks for shiny on ios/catalyst app delegates

--- ProjectTemplates/ShinyApp/Platforms/MacCatalyst/AppDelegate.cs
+++ ProjectTemplates/ShinyApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,25 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace ShinyApp;
+
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+#if usepush
+    [Export("application:didRegisterForRemoteNotificationsWithDeviceToken:")]
+    public void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        => global::Shiny.Hosting.Host.Current.Lifecycle().OnRegisteredForRemoteNotifications(deviceToken);
+
+    [Export("application:didFailToRegisterForRemoteNotificationsWithError:")]
+    public void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        => global::Shiny.Hosting.Host.Current.Lifecycle().OnFailedToRegisterForRemoteNotifications(error);
+
+    [Export("application:didReceiveRemoteNotification:fetchCompletionHandler:")]
+    public void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        => global::Shiny.Hosting.Host.Current.Lifecycle().OnDidReceiveRemoveNotification(userInfo, completionHandler);	
+#endif
 }
