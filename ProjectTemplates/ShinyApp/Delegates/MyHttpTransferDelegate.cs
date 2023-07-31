5c11d63255511edc634886f5a65e89c017dc7186
Allan Ritchie <allan.ritchie@gmail.com>
2023-07-31 01:03:48 -0400

    Updating http transfers

--- ProjectTemplates/ShinyApp/Delegates/MyHttpTransferDelegate.cs
+++ ProjectTemplates/ShinyApp/Delegates/MyHttpTransferDelegate.cs
@@ -25,11 +25,41 @@ public partial class MyHttpTransferDelegate : IHttpTransferDelegate
 
 //-:cnd:noEmit
 #if ANDROID
-public partial class MyHttpTransferDelegate : IAndroidForegroundServiceDelegate
+public partial class MyHttpTransferDelegate : IAndroidHttpTransferDelegate
 {
-    public void Configure(AndroidX.Core.App.NotificationCompat.Builder builder)
+    public void ConfigureNotification(AndroidX.Core.App.NotificationCompat.Builder builder, HttpTransfer transfer)
     {
-        
+        // switch (transfer.Status)
+        // {
+        //     case HttpTransferState.Pending:
+        //         if (transfer.Request.IsUpload)
+        //         {
+        //             builder.SetContentText($"Starting Upload {Path.GetFileName(transfer.Request.LocalFilePath)} to {transfer.Request.Uri}");
+        //         }
+        //         else
+        //         {
+        //             builder.SetContentText($"Start Download from {transfer.Request.Uri}");
+        //         }
+        //         break;
+
+        //     case HttpTransferState.Paused:
+        //     case HttpTransferState.PausedByNoNetwork:
+        //     case HttpTransferState.PausedByCostedNetwork:
+        //         var type = transfer.Request.IsUpload ? "Upload" : "Download";
+        //         builder.SetContentText($"Paused {type} for {transfer.Request.Uri}");
+        //         break;
+
+        //     case HttpTransferState.InProgress:
+        //         if (transfer.Request.IsUpload)
+        //         {
+        //             builder.SetContentText($"Uploading {Path.GetFileName(transfer.Request.LocalFilePath)}");
+        //         }
+        //         else
+        //         {
+        //             builder.SetContentText($"Downloading file from {transfer.Request.Uri}");
+        //         }
+        //         break;
+        // }
     }
 }
 #endif
