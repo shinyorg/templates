583f420b5a7c3cc385b91ece9b104acfd2eddcc6
Allan Ritchie <allan.ritchie@gmail.com>
2026-02-21 09:21:21 -0500

    Fixes

--- ProjectTemplates/ShinyApp/Infrastructure/AppHttpRequestDecorator.cs
+++ ProjectTemplates/ShinyApp/Infrastructure/AppHttpRequestDecorator.cs
@@ -1,23 +1,29 @@
 using System.Net.Http.Headers;
+using System.Globalization;
 using Shiny.Mediator.Http;
 
 namespace ShinyApp.Infrastructure;
 
 
 [Singleton]
-public class AppHttpRequestDecorator : IHttpRequestDecorator
+public class AppHttpRequestDecorator(TimeProvider timeProvider) : IHttpRequestDecorator
 {
-    public async Task Decorate(HttpRequestMessage httpMessage, IMediatorContext context, CancellationToken cancellationToken)
+    public async Task Decorate(
+        HttpRequestMessage httpMessage, 
+        IMediatorContext context, 
+        CancellationToken cancellationToken
+    )
     {
         var appInfo = AppInfo.Current;
         var deviceInfo = DeviceInfo.Current;
-        
-        httpMessage.Headers.Add("X-App-Version", appInfo.VersionString);
+
+        var locale = CultureInfo.CurrentCulture?.Name ?? string.Empty;
+        var timezone = TimeZoneInfo.Local?.Id ?? string.Empty;
+
+        httpMessage.Headers.UserAgent.ParseAdd($"{appInfo.PackageName}/{appInfo.VersionString} ({deviceInfo.Platform}; {deviceInfo.Model}; {deviceInfo.Manufacturer})");
         httpMessage.Headers.Add("X-App-Build", appInfo.BuildString);
-        httpMessage.Headers.Add("X-Device-Platform", deviceInfo.Platform.ToString());
-        httpMessage.Headers.Add("X-Device-Version", deviceInfo.Version.ToString());
-        httpMessage.Headers.Add("X-Device-Model", deviceInfo.Model);
-        httpMessage.Headers.Add("X-Device-Manufacturer", deviceInfo.Manufacturer);
+        httpMessage.Headers.Add("X-Device-Locale", locale);
+        httpMessage.Headers.Add("X-Device-Timezone", timezone);
 
         // await authService.TryRefresh();
 
